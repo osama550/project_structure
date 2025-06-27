0182f5c42efd857971801a51f8542aca5d0b075e
osama550 <osamakamel758@gmail.com>
2025-06-28 02:41:52 +0300

    Refactor EndPoints class by removing unused API endpoints and updating item method

--- lib/core/databases/api/end_points.dart
+++ lib/core/databases/api/end_points.dart
@@ -1,9 +1,5 @@
 class EndPoints {
   static const String baseUrl = 'http://finemenu.net:8000/api/';
-  static String restaurantWithId(int id) => 'webapp/restaurant/$id';
-  static const String login = 'auth/login';
+  static String item(int id) => 'item/$id';
   static const String home = 'webapp/home';
-  static const String update = 'update';
-  static const String sendcode = 'auth/send-code';
-  static const String validatecode = 'auth/validate-code';
 }
